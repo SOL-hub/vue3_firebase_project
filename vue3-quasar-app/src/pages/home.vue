91114ddd38b22d6c12f74f1f51ecca1192dba029
parksol <thfdl0317@naver.com>
2023-11-06 04:58:16 +0900

    feat : Auto Page 설정 : unplugin-vue-router

---
+++ vue3-quasar-app/src/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <div class="text-h4">{{ $route }}</div> -->
+  <div class="text-h4">{{ $route.name }}</div>
+  <div class="text-h4">{{ $route.meta }}</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
+
+<route lang="yaml">
+name: home-page
+meta:
+  requiresAuth: true
+</route>
