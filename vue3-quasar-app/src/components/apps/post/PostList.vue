a8b1f151a386842c792a028491fd6c933657b3af
parksol <thfdl0317@naver.com>
2023-11-06 13:41:41 +0900

    style : 커뮤니티 UI 정렬 및 데이터 배치

---
+++ vue3-quasar-app/src/components/apps/post/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-list bordered separator>
+    <!-- <q-item v-for="item in items" :key="item.id" clickable>
+      <q-item-section>{{ item.id }} - {{ item.title }}</q-item-section>
+    </q-item> -->
+    <!-- <postItem
+      v-for="item in items"
+      :key="item.id"
+      :id="item.id"
+      :item="item.title"
+    /> -->
+
+    <postItem v-for="item in items" :key="item.id" v-bind="item" />
+  </q-list>
+</template>
+
+<script setup>
+import PostItem from './PostItem.vue';
+defineProps({
+  items: Array,
+  default: () => [],
+});
+</script>
+
+<style lang="scss" scoped></style>
--- vue3-quasar-app/src/pages/index.vue
+++ vue3-quasar-app/src/pages/index.vue
@@ -2,15 +2,22 @@
   <q-page padding>
     <div class="text-h4">커뮤니티 목록</div>
     <section class="q-gutter-y-sm q-mt-lg">
-      <q-card v-for="id in 100" :key="id" @click="goPostDetails(id)">
-        <q-card-section>{{ id }}게시글</q-card-section>
-      </q-card>
+      <q-list bordered separator>
+        <!-- <q-item v-for="post in posts" :key="post.id" clickable>
+          <q-item-section>{{ post.id }} - {{ post.title }}</q-item-section>
+        </q-item> -->
+        <PostList :items="posts" />
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
+        quo facilis eligendi distinctio neque. Mollitia, eaque enim officiis
+        magnam aut esse voluptates maiores et quod nesciunt ipsum sunt ut
+        repellat.
+      </q-list>
     </section>
-    <ul>
+    <!-- <ul>
       <li>
         <router-link to="/posts/1">1번 게시글</router-link>
       </li>
-    </ul>
+    </ul> -->
   </q-page>
 </template>
 
@@ -18,8 +25,23 @@
 import { route } from 'quasar/wrappers';
 import { useRouter } from 'vue-router';
 
+import PostList from 'src/components/apps/post/PostList.vue';
+
 const router = useRouter();
 const goPostDetails = id => router.push(`/posts/${id}`);
+
+const posts = Array.from(Array(20), (_, index) => ({
+  id: 'index',
+  title: 'vue3 test' + index,
+  content:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur quo facilis eligendi distinctio neque. Mollitia, eaque enim officiis magnam aut esse voluptates maiores et quod nesciunt ipsum sunt ut',
+  likeCount: 3,
+  readCount: 1,
+  bookmarkCount: 4,
+  tags: ['html', 'css', 'javascript'],
+  uid: 'uid',
+  category: '카테고리' + index,
+}));
 </script>
 
 <style lang="scss" scoped></style>
