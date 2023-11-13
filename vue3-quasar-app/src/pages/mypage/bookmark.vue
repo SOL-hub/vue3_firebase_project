8bd1169d99a588d06c3fcf841a68123b5f867d0c
parksol <thfdl0317@naver.com>
2023-11-14 01:57:16 +0900

    add :  중첩 레이아웃 구성 vueRouter

--- vue3-quasar-app/src/pages/mypage/bookmark.vue
+++ vue3-quasar-app/src/pages/mypage/bookmark.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>
-    <div class="text-h5">Mypage/bookmark</div>
-  </div>
+  <PostList :items="posts" />
 </template>
 
-<script>
-export default {};
+<script setup>
+import PostList from 'src/components/apps/post/PostList.vue';
+
+const posts = Array.from(Array(3), (_, index) => ({
+  id: 'A' + index,
+  title: 'vue3 test' + index,
+  content:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur quo facilis eligendi distinctio neque. Mollitia, eaque enim officiis magnam aut esse voluptates maiores et quod nesciunt ipsum sunt ut',
+  likeCount: 3,
+  readCount: 1,
+  bookmarkCount: 4,
+  tags: ['html', 'css', 'javascript'],
+  uid: 'uid',
+  category: '카테고리' + index,
+}));
 </script>
 
 <style lang="scss" scoped></style>
