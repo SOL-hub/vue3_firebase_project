28d1343ae9e080cfca49ea049d5e4cbed10f59f9
parksol <thfdl0317@naver.com>
2023-11-14 01:13:41 +0900

    add : multiple v-model 기반 form 컴포넌트 적용

--- vue3-quasar-app/src/components/apps/post/PostWriteDialog.vue
+++ vue3-quasar-app/src/components/apps/post/PostWriteDialog.vue
@@ -6,36 +6,13 @@
         <q-btn v-close-popup flat round dense icon="close" />
       </q-toolbar>
       <q-separator />
-      <q-form class="q-pa-md q-gutter-y-sm">
-        <q-input v-model="form.title" outlined placeholder="제목" />
-        <q-select v-model="form.category" outlined :option="categories">
-          <template v-if="!form.category" #selected>
-            <span class="text-grey-7">카테고리를 선택하세요.</span>
-          </template>
-        </q-select>
-        <q-input
-          v-model="form.content"
-          type="textarea"
-          outlined
-          placeholder="내용을 작성해주세여"
-        />
-        <q-input
-          outlined
-          placeholder="태그를 작성해주세여(입력 후 엔터)"
-          prefix="#"
-          v-model="tagField"
-        />
-        <q-chip outline dense color="teal" removable @remove="removeTag"
-          >test</q-chip
-        >
-      </q-form>
-
+      <PostForm
+        v-model="form.title"
+        v-model:title="form.title"
+        v-model:category="form.category"
+        v-model:content="form.content"
+      />
       <q-separator />
-
-      <q-card-actions align="right">
-        <q-btn flat label="취소하기" color="primary" v-close-popup />
-        <q-btn type="submit" flat label="저장하기" color="primary" />
-      </q-card-actions>
     </q-card>
   </q-dialog>
 </template>
@@ -51,18 +28,9 @@ const getInitialForm = () => ({
 
 <script setup>
 import { ref } from 'vue';
-import StickySideBar from 'src/components/StickySideBar.vue';
-import { getCategories } from 'src/services/category';
-
-const categories = getCategories();
+import PostForm from 'src/components/apps/post/PostForm.vue';
 
 const form = ref(getInitialForm());
-
-const tagField = ref('');
-
-const removeTag = () => {
-  console.log('removeTag');
-};
 
 const onHide = () => {
   form.value = getInitialForm();
--- vue3-quasar-app/src/pages/posts/[id]/components/PostDetails.vue
+++ vue3-quasar-app/src/pages/posts/[id]/components/PostDetails.vue
@@ -22,20 +22,23 @@
         <div>짐코딩</div>
         <div class="text-grey-6">2일 전</div>
       </div>
-      <q-space>
-        <q-btn icon="more_horiz" round flat>
-          <q-menu>
-            <q-list style="min-width: 100px">
-              <q-item clickable v-close-popup>
-                <q-item-section>수정하기</q-item-section>
-              </q-item>
-              <q-item clickable v-close-popup>
-                <q-item-section>삭제하기</q-item-section>
-              </q-item>
-            </q-list>
-          </q-menu>
-        </q-btn>
-      </q-space>
+      <q-space />
+      <q-btn icon="more_horiz" round flat>
+        <q-menu>
+          <q-list style="min-width: 100px">
+            <q-item
+              clickable
+              v-close-popup
+              :to="`/posts/${$route.params.id}/edit`"
+            >
+              <q-item-section>수정하기</q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup>
+              <q-item-section>삭제하기</q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
     </div>
     <div class="q-mt-md text-h5 text-weight-bold">제목입니다.</div>
     <div class="row items-center q-gutter-x-md q-mt-md justify-end">
